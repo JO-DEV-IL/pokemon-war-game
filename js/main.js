f4ede23caab91aa4b17d261197f02f391af1de7e
JO-DEV-IL <jillianokeefe.dev@gmail.com>
2022-09-28 20:12:52 -0400

    learn how the api works

--- js/main.js
+++ js/main.js
@@ -9,10 +9,17 @@ async function apiRequest(){
         }else{
             const data = await response.json() // if good then retrieve the data
             
-            console.log(data.data[0])
+            console.log(data) // print data from api
             
+            //Pokemon 1 data
             document.querySelector('#p1').innerText = data.data[0].name
+            const pokemonOneImage = document.querySelector('#p1-img')
+            pokemonOneImage.src = data.data[0].images.small
+            
+            // Pokemon 2 data
             document.querySelector('#p2').innerText = data.data[1].name
+            const pokemonTwoImage = document.querySelector('#p2-img')
+            pokemonTwoImage.src = data.data[1].images.small
         }
     }catch(error){
         console.log(error) //catch and print any errors
