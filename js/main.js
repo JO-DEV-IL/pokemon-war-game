5bc437cf2aacce11fe4ff3f0383309d9dd310479
Jillian O'Keefe <jillianokeefe.dev@gmail.com>
2022-09-29 11:04:47 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -11,33 +11,65 @@ async function apiRequest(){
             
             // console.log(data) // print data from api
 
-            // Randomize pokemon cards
+            
+            
+            // RANDOMIZE POKEMON CHOICES //
+            
+            // Randomize pokemon cards and assign variables
             const randomNumber1 = Math.floor(Math.random() * 250)
             const randomNumber2 = Math.floor(Math.random() * 250)
             console.log(data.data[randomNumber1])
             console.log(data.data[randomNumber2])
 
-            //Pokemon 1 data
-            // document.querySelector('#p1').innerText = data.data[randomNumber1].name
+            // Pokemon 1 data
             const pokemonOneImage = document.querySelector('#p1-img')
+            const pokemonOneName = data.data[randomNumber1].name
             pokemonOneImage.src = data.data[randomNumber1].images.small
             
             // Pokemon 2 data
-            // document.querySelector('#p2').innerText = data.data[randomNumber2].name
             const pokemonTwoImage = document.querySelector('#p2-img')
+            const pokemonOneTwo = data.data[randomNumber2].name
             pokemonTwoImage.src = data.data[randomNumber2].images.small
 
-            //Randomize pokemon battle
-            const p1m1 = document.querySelector('#pokemon1move1')
-            // const p2m1 = document.querySelector('#pokemon2move1')
-            // const p1m2 = document.querySelector('#pokemon1move2')
-            // const p2m2 = document.querySelector('#pokemon2move2')
-            const attacksArray = data.data[randomNumber1].attacks
-            const abilityArray = data.data[randomNumber1].abilities
-            const randomAttack = Math.floor(Math.random() * attacksArray.length)
-            const randomAbility = Math.floor(Math.random() * abilityArray.length)
+            
+            // POKEMON BATTLE RANDOMIZER //
 
-            p1m1.innerText = `${data.data[randomNumber1].name} uses ${data.data[randomNumber1]}!`
+            // Assign move variables
+            const p1m1 = document.querySelector('#pokemon1move1')
+            const p2m1 = document.querySelector('#pokemon2move1')
+            const p1m2 = document.querySelector('#pokemon1move2')
+            const p2m2 = document.querySelector('#pokemon2move2')
+            
+            // Assign attacks/abilities variables for Pokemon 1
+            const attacksArrayPokemonOne = data.data[randomNumber1].attacks
+            const abilityArrayPokemonOne = data.data[randomNumber1].abilities
+            
+            // Assign randomizing variables
+            const randomAttackPokemonOne = Math.floor(Math.random() * attacksArrayPokemonOne.length)
+            const randomAbilityPokemonOne = Math.floor(Math.random() * abilityArrayPokemonOne.length)
+            
+            const attackOrAbilityArray = [randomAbilityPokemonOne, randomAttackPokemonOne]
+            const attackOrAbility = Math.floor(Math.random() * attackOrAbilityArray.length)
+            
+            // Handle Pokemon 1 moves
+            function pokemonOneMoves(){
+                // Does it have both abilities and attacks?
+                if(attacksArrayPokemonOne && abilityArrayPokemonOne){
+                    // If it does, randomly choose one to use and randomize its array
+                    // PRETTY SURE THIS WON'T WORK THE WAY I WANT IT TO?
+                    return data.data[randomNumber1].attackOrAbility[attackOrAbility]
+                // if it doesn't have both, does it have attacks?
+                }else if(attacksArrayPokemonOne){
+                    // if yes then randomize the attack array
+                    return `${pokemonOneName} uses ${randomAttackPokemonOne}!`
+                }else if(abilityArrayPokemonOne){
+                    // else it randomizes the abilities array
+                    return `${pokemonOneName} uses ${randomAbilityPokemonOne}!`
+                }else{
+                    // if none of the above are satisfied something might be borked
+                    console.log('I dont even know what happened man')
+                }
+            }
         }
     }catch(error){
         console.log(error) //catch and print any errors
