f9a9a25cdf94f02615bdc2b5a6454c6f89a750f2
JO-DEV-IL <jillianokeefe.dev@gmail.com>
2022-09-28 20:38:29 -0400

    randomizer setup

--- js/main.js
+++ js/main.js
@@ -10,16 +10,20 @@ async function apiRequest(){
             const data = await response.json() // if good then retrieve the data
             
             console.log(data) // print data from api
-            
+
+            // Randomize pokemon cards
+            const randomNumber1 = Math.floor(Math.random() * 250)
+            const randomNumber2 = Math.floor(Math.random() * 250)
+
             //Pokemon 1 data
-            document.querySelector('#p1').innerText = data.data[0].name
+            document.querySelector('#p1').innerText = data.data[randomNumber1].name
             const pokemonOneImage = document.querySelector('#p1-img')
-            pokemonOneImage.src = data.data[0].images.small
+            pokemonOneImage.src = data.data[randomNumber1].images.small
             
             // Pokemon 2 data
-            document.querySelector('#p2').innerText = data.data[1].name
+            document.querySelector('#p2').innerText = data.data[randomNumber2].name
             const pokemonTwoImage = document.querySelector('#p2-img')
-            pokemonTwoImage.src = data.data[1].images.small
+            pokemonTwoImage.src = data.data[randomNumber2].images.small
         }
     }catch(error){
         console.log(error) //catch and print any errors
